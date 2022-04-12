cd17cf88e7a6f30d48376c2a2db729f64ccfaa2e
CHris Nussbaum <chrisnussbaum@gmail.com>
2022-04-11 21:40:12 -0500

    Fix at home comparison

--- src/NuttyTree.NetDaemon.Waze/Models/LocationCoordinates.cs
+++ src/NuttyTree.NetDaemon.Waze/Models/LocationCoordinates.cs
@@ -2,7 +2,7 @@
 
 namespace NuttyTree.NetDaemon.Waze.Models
 {
-    public class LocationCoordinates
+    public class LocationCoordinates : IEquatable<LocationCoordinates>
     {
         public static LocationCoordinates Empty { get; } = new LocationCoordinates { Latitude = 0, Longitude = 0 };
 
@@ -12,7 +12,9 @@ namespace NuttyTree.NetDaemon.Waze.Models
         [JsonPropertyName("lon")]
         public double Longitude { get; set; }
 
-        public override bool Equals(object? obj) => obj is LocationCoordinates coordinates && Latitude == coordinates.Latitude && Longitude == coordinates.Longitude;
+        public bool Equals(LocationCoordinates? other) => other?.Latitude == Latitude && other?.Longitude == Longitude;
+
+        public override bool Equals(object? obj) => Equals(obj as LocationCoordinates);
 
         public override int GetHashCode() => HashCode.Combine(Latitude, Longitude);
     }
