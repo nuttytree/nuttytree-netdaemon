afce37ba50026cf1bf3dbd3826542af03f3c6dde
Chris Nussbaum <chrisnussbaum@gmail.com>
2024-01-28 16:58:35 -0600

    Add a test gRPC endpoint (#51)

--- src/NuttyTree.NetDaemon.Application/ElectronicsTime/IServiceCollectionExtensions.cs
+++ src/NuttyTree.NetDaemon.Application/ElectronicsTime/IServiceCollectionExtensions.cs
@@ -3,12 +3,14 @@ using NuttyTree.NetDaemon.Application.ElectronicsTime.Options;
 
 namespace NuttyTree.NetDaemon.Application.ElectronicsTime;
 
-public static class IServiceColectionExtensions
+public static class IServiceCollectionExtensions
 {
     public static IServiceCollection AddElectronicsTimeApp(this IServiceCollection services)
     {
         services.AddOptions<ElectronicsTimeOptions>()
             .BindConfiguration(nameof(ElectronicsTime));
+
+        services.AddGrpc();
 
         return services;
     }
