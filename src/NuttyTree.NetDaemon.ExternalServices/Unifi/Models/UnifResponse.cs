a6e8858655d35b88e76510f3764ce5187ece8e5d
Chris Nussbaum <chrisnussbaum@gmail.com>
2026-03-24 21:56:19 -0500

    Remove the guest net password update app and NuGet updates

--- src/NuttyTree.NetDaemon.ExternalServices/Unifi/Models/UnifResponse.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace NuttyTree.NetDaemon.ExternalServices.Unifi.Models;
-
-public sealed class UnifResponse<T>
-    where T : new()
-{
-    public UnifiMetadata Meta { get; set; } = new();
-
-    public ICollection<T> Data { get; set; } = [];
-}
